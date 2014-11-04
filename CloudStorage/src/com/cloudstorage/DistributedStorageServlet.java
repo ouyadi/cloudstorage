87b68abe7b108721bafc7e878a5e0357a03584c0
ouyadi <ouyadi@gmail.com>
2014-11-04 05:31:15 -0600

    fix upload fix download

--- CloudStorage/src/com/cloudstorage/DistributedStorageServlet.java
+++ CloudStorage/src/com/cloudstorage/DistributedStorageServlet.java
@@ -1,44 +1,127 @@
 package com.cloudstorage;
 
-import java.io.IOException;
-import java.nio.ByteBuffer;
-import javax.servlet.ServletException;
-import javax.servlet.http.*;
+import com.google.appengine.api.blobstore.BlobKey;
+import com.google.appengine.api.blobstore.BlobstoreService;
+import com.google.appengine.api.blobstore.BlobstoreServiceFactory;
+import com.google.appengine.tools.cloudstorage.GcsFileOptions;
+import com.google.appengine.tools.cloudstorage.GcsFilename;
+import com.google.appengine.tools.cloudstorage.GcsInputChannel;
+import com.google.appengine.tools.cloudstorage.GcsOutputChannel;
+import com.google.appengine.tools.cloudstorage.GcsService;
+import com.google.appengine.tools.cloudstorage.GcsServiceFactory;
+import com.google.appengine.tools.cloudstorage.RetryParams;
 
+import org.apache.commons.fileupload.FileItemStream;
+import org.apache.commons.fileupload.FileItemIterator;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
 
-import com.google.appengine.tools.cloudstorage.GcsFilename;
+
+
+
+import org.apache.commons.io.IOUtils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.StringWriter;
+import java.nio.channels.Channels;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @SuppressWarnings("serial")
 public class DistributedStorageServlet extends HttpServlet {
-	private ServletFileUpload uploader = null;
 	
+	public static final boolean SERVE_USING_BLOBSTORE_API = false;
+	
+	
+	private final GcsService gcsService = GcsServiceFactory.createGcsService(new RetryParams.Builder()
+    .initialRetryDelayMillis(10)
+    .retryMaxAttempts(10)
+    .totalRetryPeriodMillis(15000)
+    .build());
+	
+	private static final int BUFFER_SIZE = 100 * 1024 * 1024;
 	
 	@Override
-	public void init() throws ServletException{
+	  public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+	    GcsFilename fileName = getFileName(req);
+	    GcsInputChannel readChannel = gcsService.openPrefetchingReadChannel(fileName, 0, BUFFER_SIZE);
+	    copy(Channels.newInputStream(readChannel), resp.getOutputStream());
+	    String mimeType = gcsService.getMetadata(fileName).getOptions().getMimeType();
+	    if(mimeType==null){
+	    	mimeType = "application/octet-stream";
+	    }
+	    resp.setContentType(mimeType);
+	    String headerKey = "Content-Disposition";
+	    String headerValue = String.format("attachment;filename=\"%s\"", fileName.getObjectName());
+	    resp.setHeader(headerKey, headerValue);
+	  }
+	
+	@Override
+	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws  IOException, ServletException {
+		
+		
+		String action="";
+		try {
+		      ServletFileUpload upload = new ServletFileUpload();
+		      resp.setContentType("text/plain");
+		      
+		      StringWriter writer = new StringWriter();
+		      
+		      FileItemIterator iterator = upload.getItemIterator(req);
+		      while (iterator.hasNext()) {
+		        FileItemStream item = iterator.next();
+		        InputStream stream = item.openStream();
+
+		        if (item.isFormField()) {
+		        	if(item.getContentType()==null){
+		        		continue;
+		        	}
+		        	else if(item.getFieldName().equals("action")){
+		        		IOUtils.copy(stream, writer);
+		        		action = writer.toString();
+		        	}
+		        } else {
+		        	GcsFilename filename = new GcsFilename("my-project",item.getName());
+		        	GcsOutputChannel outputChannel = gcsService.createOrReplace(filename, GcsFileOptions.getDefaultInstance());
+		        	copy(stream, Channels.newOutputStream(outputChannel));
+		        }
+		      }
+		    } catch (Exception ex) {
+		      throw new ServletException(ex);
+		    }
+		
+		
+		
 		
 	}
 	
-	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		/*
-		StringBuilder builder = new StringBuilder();
-		BufferedReader br = new BufferedReader(new InputStreamReader(request.getInputStream()));
-		String json = "";
-		while ((json = br.readLine()) != null) {
-		builder.append(json);
-		}
-		String result = builder.toString();
-		*/
-		String action = request.getParameter("action");
-		if(action.equals("insert")){
-			StorageHandler handler = new StorageHandler();
-			String key = request.getParameter("key");
-			GcsFilename name = new GcsFilename("project-bucket",key);
-			HttpSession session = request.getSession();
-			byte[] content = (byte[]) session.getAttribute("buffer");
-			handler.writeToFile(name, content);
-		}
+	private GcsFilename getFileName(HttpServletRequest req) {
+	    String[] splits = req.getRequestURI().split("/", 4);
+	    if (!splits[0].equals("") || !splits[1].equals("gcs")) {
+	      throw new IllegalArgumentException("The URL is not formed as expected. " +
+	          "Expecting /upload/<bucket>/<object>");
+	    }
+	    return new GcsFilename(splits[2], splits[3]);
 	}
+	
+	private void copy(InputStream input, OutputStream output) throws IOException {
+	    try {
+	      byte[] buffer = new byte[BUFFER_SIZE];
+	      int bytesRead = input.read(buffer);
+	      while (bytesRead != -1) {
+	        output.write(buffer, 0, bytesRead);
+	        bytesRead = input.read(buffer);
+	      }
+	    } finally {
+	      input.close();
+	      output.close();
+	    }
+	}
+	
 	
 	
 
--- CloudStorage/war/index.jsp
+++ CloudStorage/war/index.jsp
@@ -9,51 +9,68 @@
 <script  src="http://malsup.github.io/jquery.form.js"></script>
 </head>
 	<body>
-    <h1>Distributed Storage System</h1>
-	<div id="result"></div>
-	<form id="form" enctype="multipart/form-data">
+    <h1>Distributed Storage System</h1><br>
+	<div id="result" name="result"></div><br>
+	<form id="putFile" name="putFile" enctype="multipart/form-data">
 	    <table>
 	      <tr>
 	        <td colspan="2" style="font-weight:bold;">Insert:</td>        
 	      </tr>
 	      <tr>	      
-	        <td>Key:<input type="text" id="insertKey" name="key"/></td>
-	        <td>File:<input type="file" id="insertValue" name="file"/></td>
-	        <td><input type="button" value="Insert" id="insertValue" onClick="insert(event)"/></td>	      
+	        <td>File:<input type="file" id="fileName" name="fileName"/></td>
+	        <td><input type="button" value="Insert" id="insertValue" name="insertButton" onClick="insert()"/></td>	      
 	      </tr>
 	    </table>
 	    <input type="hidden" id="action" name="action"/>
     </form>
     
-    <form id="fileholder" enctype="multipart/form-data" >
-    	<input type="file" id="upload" style="display:none"/>
-    
+    <form action="/index.html" method="get" name="submitGet">
+          <table>
+          <tr>
+          	<td><input type="text" name ="fileName"/></td>
+            <td><input type="submit" onclick='changeGetPath(this)' value="Download Content" name="download"/></td>
+            </tr>
+          </table>
     </form>
+    
+    
     <script type="text/javascript">
-    	function insert(event){
-    		event.preventDefault();
+    	var bucket = "my-project";
+    	function insert(){
     		$("#action").val("insert");
-    		var data = new FormData();
-    		var file = $("#insertValue")[0].files[0];
-    		data.append("file",file);
-	        $.ajax({
-	        	url:'/uploadfile',
-	        	data:data,
-	        	type:'post',
-	        	cache: false,
-    			processData: false,
-    		    contentType: false
-	        }).done( function(result){
-		        var form = $("#form");
-		        $.ajax({
-		        	url:'/distributedstorage',
-		        	data:form.serialize(),
-		        	type:'post'
-		        });
-		     });
-	        
     		
+            var filename = document.forms["putFile"]["fileName"].value;
+            if (bucket == null || bucket == "" || filename == null || filename == "") {
+              alert("Both Bucket and FileName are required");
+              return false;
+            } else {
+              var postData = new FormData(document.forms["putFile"]);
+              var request = new XMLHttpRequest();
+              request.open("POST", "/gcs/" + bucket + "/" + filename, false);
+              //request.setRequestHeader("Content-Type", "text/plain;charset=UTF-8");
+              request.send(postData);
+              $('#result').innerHTML = "Uploading";
+              if(request.response!=null){
+            	  document.forms["putFile"]["fileName"].value="";
+            	  $('#result')[0].innerHTML = "Insert Complete";
+              }
+              else{
+            	  document.forms["putFile"]["fileName"].value="";
+            	  $('#result')[0].innerHTML = "Insert Failure";
+              }
+            }
     	}
+    	
+    	function changeGetPath() {
+            var filename = document.forms["submitGet"]["fileName"].value;
+            if (bucket == null || bucket == "" || filename == null || filename == "") {
+              alert("Both Bucket and FileName are required");
+              return false;
+            } else {
+              document.submitGet.action = "/gcs/" + bucket + "/" + filename;
+            }
+        }
+    	
     
     </script>
   </body>
